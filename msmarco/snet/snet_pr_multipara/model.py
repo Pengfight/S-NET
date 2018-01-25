ca14c18250e5687c519962d681287b4807784d8d
burglarhobbit <bhavyapatwa007@gmail.com>
2018-01-26 02:16:57 +0530

    updated snet passage ranking

--- msmarco/snet/snet_pr_multipara/model.py
+++ msmarco/snet/snet_pr_multipara/model.py
@@ -213,7 +213,7 @@ class Model(object):
 						logits=gi_, labels=tf.reshape(self.pr,[-1,1]))
 			self.losses3 = tf.Print(self.losses3,[self.losses3],message="losses3:")
 			temp = tf.reduce_max(self.losses3)
-			temp = tf.Print(temp,[temp],message="temp:")
+			temp = tf.Print(temp,[temp,"msg"],message="temp:")
 			self.pr_loss = tf.reduce_mean(self.losses3)
 			#self.pr_loss = tf.Print(self.pr_loss,[self.pr_loss])
 			#assert(self.pr_loss.get_shape().as_list() == self.loss.get_shape().as_list())
