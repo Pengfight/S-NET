b89cbf22f627bcf3ed2de8c3e89d0feed705956c
burglarhobbit <bhavyapatwa007@gmail.com>
2018-01-25 17:56:46 +0530

    updated snet passage ranking

--- msmarco/snet/snet_pr_multipara/model.py
+++ msmarco/snet/snet_pr_multipara/model.py
@@ -210,7 +210,7 @@ class Model(object):
 			self.losses3 = tf.nn.softmax_cross_entropy_with_logits(
 						logits=gi, labels=tf.reshape(self.pr,[-1,1]))
 			self.pr_loss = tf.reduce_mean(self.losses3)
-			self.pr_loss = tf.Print(self.pr_loss,[self.pr_loss])
+			#self.pr_loss = tf.Print(self.pr_loss,[self.pr_loss])
 			#assert(self.pr_loss.get_shape().as_list() == self.loss.get_shape().as_list())
 			self.r = tf.get_variable("r", [1])
 			self.e_loss1 = tf.multiply(self.r,self.loss)
