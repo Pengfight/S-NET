134240c90556987484a88d027dc080ae6cfa8d69
burglarhobbit <bhavyapatwa007@gmail.com>
2018-02-01 01:33:28 +0530

    structural update

--- msmarco/rnet/local_span_with_var_summary/main.py
+++ msmarco/rnet/local_span_with_var_summary/main.py
@@ -47,6 +47,7 @@ def train(config):
 		writer = tf.summary.FileWriter(config.log_dir, graph=tf.get_default_graph())
 		sess.run(tf.global_variables_initializer())
 		saver = tf.train.Saver(max_to_keep=config.max_checkpoint_to_keep)
+		print(config.save_dir)
 		saver.restore(sess, tf.train.latest_checkpoint(config.save_dir))
 		#saver.restore(sess, tf.train.latest_checkpoint(config.save_dir))
 		train_handle = sess.run(train_iterator.string_handle())
